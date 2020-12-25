0290d902dea6e881367683e45a98101691d3d56f
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-25 00:16:45 -0500

    Add blur effect to the card deck.

--- src/components/AnswerCardDeck.js
+++ src/components/AnswerCardDeck.js
@@ -1,9 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Grid } from "@material-ui/core";
+import { Box, Grid, makeStyles } from "@material-ui/core";
 import AnswerCard from "./AnswerCard";
 
-let AnswerCardDeck = ({ questions, showKey = false }) => {
+const useStyles = makeStyles((theme) => ({
+  unblur: {
+    filter: "blur(0px) !important",
+  },
+  blur: {
+    filter: "blur(15px)",
+    transitionProperty: "filter",
+    transitionDuration: "2s",
+    transitionTimingFunction: "ease",
+  },
+}));
+
+let AnswerCardDeck = ({ questions, blur = true, showKey = false }) => {
+  const classes = useStyles();
+
   let cards = [];
   for (let question of questions) {
     cards.push({
@@ -15,13 +29,15 @@ let AnswerCardDeck = ({ questions, showKey = false }) => {
   }
 
   return (
-    <Grid container spacing={1}>
-      {cards.map((item) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-          <AnswerCard {...item} />
-        </Grid>
-      ))}
-    </Grid>
+    <Box className={`${classes.blur} ${!blur ? classes.unblur : ""}`}>
+      <Grid container spacing={1}>
+        {cards.map((item) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+            <AnswerCard {...item} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
--- src/components/AnswerCardDeck.stories.js
+++ src/components/AnswerCardDeck.stories.js
@@ -5,15 +5,22 @@ export default {
   title: "AnswerCardDeck",
   component: AnswerCardDeck,
   argTypes: {
-    questions: "array",
+    questions: { control: "array" },
+    blur: { control: "boolean" },
+    showKey: { control: "boolean" },
   },
 };
 
 const Template = (args) => <AnswerCardDeck {...args} />;
 
-export const Start = Template.bind({});
-Start.args = {
+export const Initial = Template.bind({});
+Initial.args = {
   questions: [
+    {
+      label: "Ubuntu 20.04",
+      answer: "Focal Fossa",
+      isCorrect: true,
+    },
     {
       label: "Ubuntu 18.04",
       answer: "Bionic Beaver",
@@ -27,7 +34,7 @@ Start.args = {
     {
       label: "Ubuntu 14.04",
       answer: "Trusty Tahr",
-      isCorrect: true,
+      isCorrect: false,
     },
     {
       label: "Ubuntu 12.04",
@@ -42,34 +49,14 @@ Start.args = {
   ],
 };
 
+export const GameInProgress = Template.bind({});
+GameInProgress.args = {
+  ...Initial.args,
+  blur: false,
+};
+
 export const GameOver = Template.bind({});
 GameOver.args = {
+  ...GameInProgress.args,
   showKey: true,
-  questions: [
-    {
-      label: "Ubuntu 18.04",
-      answer: "Bionic Beaver",
-      isCorrect: false,
-    },
-    {
-      label: "Ubuntu 16.04",
-      answer: "Xenial Xeurus",
-      isCorrect: true,
-    },
-    {
-      label: "Ubuntu 14.04",
-      answer: "Trusty Tahr",
-      isCorrect: true,
-    },
-    {
-      label: "Ubuntu 12.04",
-      answer: "Precise Pangolin",
-      isCorrect: true,
-    },
-    {
-      label: "Ubuntu 10.04",
-      answer: "Lucid Lynx",
-      isCorrect: false,
-    },
-  ],
 };
