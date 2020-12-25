4443ce0a10fcd3f07ebcdf4dbb77e8764c2f59e0
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-25 01:23:47 -0500

    WIP create an app bar.

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@material-ui/core";
+
+let NavBar = () => (
+  <AppBar position="static">
+    <Typography variant="h1">Sporclone</Typography>
+  </AppBar>
+);
