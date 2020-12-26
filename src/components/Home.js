03a31ba36e12bd914c24db98d97e56479fad326b
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-25 20:30:10 -0500

    Create a basic homepage.

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+
+let Home = () => (
+  <Box>
+    <Typography variant="h2">Welcome to Sporclone!</Typography>
+  </Box>
+);
+
+export default Home;
