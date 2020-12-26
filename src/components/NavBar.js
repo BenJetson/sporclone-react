9b525785d517add50794a25adf5babdb3d9eded3
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-26 02:28:29 -0500

    Add NavDrawer to NavBar component.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -12,6 +12,8 @@ import {
 import { Menu as MenuIcon } from "@material-ui/icons";
 import { Link as RouterLink } from "react-router-dom";
 
+import NavDrawer from "./NavDrawer";
+
 const useStyles = makeStyles((theme) => ({
   navbar: {
     marginBottom: "1.5em",
@@ -20,19 +22,32 @@ const useStyles = makeStyles((theme) => ({
 
 let NavBar = () => {
   const classes = useStyles();
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  let openDrawer = () => setDrawerOpen(true);
+  let closeDrawer = () => setDrawerOpen(false);
+
   return (
-    <AppBar position="static" className={classes.navbar}>
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="h1">
-          <Link to="/" component={RouterLink} color="inherit">
-            Sporclone
-          </Link>
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <NavDrawer open={drawerOpen} onClose={closeDrawer} />
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={openDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="h1">
+            <Link to="/" component={RouterLink} color="inherit">
+              Sporclone
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
