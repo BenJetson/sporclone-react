0a4047ad14f760040f616fb5035743a729e570b1
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-26 13:31:26 -0500

    Switch to HashRouter so the site will work with GitHub Pages.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Container, ThemeProvider } from "@material-ui/core";
 
 import Home from "./components/Home";
@@ -12,17 +12,15 @@ import GameLoader from "./components/GameLoader";
 let App = () => {
   return (
     <ThemeProvider theme={Theme}>
-      <Router>
+      <Router basename={BaseURL}>
         <NavBar />
         <Container>
-          {/* <Route path={BaseURL}> */}
           <Switch>
-            <Route path={`${BaseURL}/game/:gameId`} children={<GameLoader />} />
-            <Route path={BaseURL} key="home">
+            <Route path={"/game/:gameId"} children={<GameLoader />} />
+            <Route path={"/"} key="home">
               <Home />
             </Route>
           </Switch>
-          {/* </Route> */}
         </Container>
       </Router>
     </ThemeProvider>
