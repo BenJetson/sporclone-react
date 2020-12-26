292a84b069ac745510938952458663b4fcd38ac6
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-25 20:54:31 -0500

    Include a list of available games on the homepage.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+import GameIndex from "../games/index.json";
 
 let Home = () => (
   <Box>
     <Typography variant="h2">Welcome to Sporclone!</Typography>
+    <Typography>Select a game to play from the list below.</Typography>
+    <List>
+      {GameIndex.map((g) => (
+        <ListItem component={Link} button to={`/game/${g.id}`}>
+          <ListItemText primary={g.name} />
+        </ListItem>
+      ))}
+    </List>
   </Box>
 );
 
