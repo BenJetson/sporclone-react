883d11adb8e19e07565c8abe81490399821631b9
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-25 20:30:42 -0500

    Create app shell and define routes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container } from "@material-ui/core";
 
-function App() {
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+
+import GameIndex from "./games/index.json";
+import GameController from "./components/GameController";
+
+let App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Container>
+        <Switch>
+          {GameIndex.map((g, idx) => {
+            // TODO would be better to load these dynamically via AJAX instead
+            // of doing it at build time
+            let game = require(`./games/${g.id}.json`);
+            return (
+              <Route path={`/game/${g.id}`}>
+                <GameController
+                  gameIdx={idx}
+                  template={game}
+                  allGames={GameIndex}
+                />
+              </Route>
+            );
+          })}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
