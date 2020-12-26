7f384b5968e7cf8f980b860a4cd28bfbb4bbe4b1
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-26 00:05:22 -0500

    Support production base URL.

--- src/App.js
+++ src/App.js
@@ -9,28 +9,35 @@ import Theme from "./Theme";
 import GameIndex from "./games/index.json";
 import GameLoader from "./components/GameLoader";
 
+let baseUrl = "/sporclone-react";
+if (process.env.NODE_ENV !== "production") {
+  baseUrl = "";
+}
+
 let App = () => {
   return (
     <ThemeProvider theme={Theme}>
       <Router>
         <NavBar />
         <Container>
-          <Switch>
-            {GameIndex.map((g, idx) => {
-              return (
-                <Route path={`/game/${g.id}`} key={idx}>
-                  <GameLoader
-                    gameId={g.id}
-                    gameIdx={idx}
-                    allGames={GameIndex}
-                  />
-                </Route>
-              );
-            })}
-            <Route path="/" key="home">
-              <Home />
-            </Route>
-          </Switch>
+          <Route path={baseUrl}>
+            <Switch>
+              {GameIndex.map((g, idx) => {
+                return (
+                  <Route path={`/game/${g.id}`} key={idx}>
+                    <GameLoader
+                      gameId={g.id}
+                      gameIdx={idx}
+                      allGames={GameIndex}
+                    />
+                  </Route>
+                );
+              })}
+              <Route path="/" key="home">
+                <Home />
+              </Route>
+            </Switch>
+          </Route>
         </Container>
       </Router>
     </ThemeProvider>
