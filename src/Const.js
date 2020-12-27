1b5a4c4e6271664364f9b978c211f578894a2fd9
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-27 16:30:14 -0500

    Fix BaseURL since CRA local server respects homepage.

--- src/Const.js
+++ src/Const.js
@@ -1,4 +1,7 @@
-export let BaseURL = "/sporclone-react";
-if (process.env.NODE_ENV !== "production") {
-  BaseURL = "";
-}
+export const BaseURL = "/sporclone-react";
+
+// The below is no longer necessary since "homepage" was set in package.json.
+//
+// if (process.env.NODE_ENV !== "production") {
+//   BaseURL = "";
+// }
